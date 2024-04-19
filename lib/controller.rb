0eedb187c4871f0889f30f93f861662b1ed230a3
root <root@TL>
2024-04-19 18:24:40 +0200

    second commit

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,28 +3,29 @@ require_relative 'router'
 require_relative 'view'
 
 class Controller
-
     def initialize
-        @view = View.new
+      @view = View.new
     end
-
+  
     def create_gossip
-        params = @view.create_gossip
-        gossip = Gossip.new(params[:author], params[:content])
-        gossip.save_csv
+      params = @view.create_gossip
+      gossip = Gossip.new(params[:author], params[:content], @view.number)
+      gossip.save_csv
     end
-
+  
     def index_gossips
-        puts "Affichage de tous les gossips :"
-        Gossip.all.each do |gossip|
-          puts "#{gossip.author} : #{gossip.content}"
-        end
+      puts "Affichage de tous les gossips :"
+      Gossip.all.each do |gossip|
+        puts "#{gossip.number} : #{gossip.author} : #{gossip.content}"
+      end
     end
-
+  
     def destroy_gossip
         puts "Entrez le nom de l'auteur du gossip que vous souhaitez supprimer :"
         author = gets.chomp
+        puts "Entrez le numéro du gossip que vous souhaitez supprimer :"
+        number = gets.chomp
     
-        Gossip.destroy(author)
-      end
+        Gossip.destroy(author, number)
+    end
 end
--- lib/view.rb
+++ lib/view.rb
@@ -2,12 +2,18 @@ require_relative 'controller'
 require_relative 'router'
 
 class View
+attr_reader :number
+
+   def initialize
+    @number = 0  # Initialisation de number à 0
+   end
 
     def create_gossip
         puts "Quel est ton nom, gossipeur ?"
         input1 = gets.chomp
         puts " Et le contenu de ton gossip ?"
         input2 = gets.chomp
+        @number += 1
         return params = {author:input1, content:input2}
     end
 
